f01af4adf2e221ace825822cc09c43094017c1ab
calex <calexch@tuta.com>
2025-05-06 16:22:42 -0500

    Main now parses YAML config and pulls posts from listed communites

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+import yaml
 import requests
 
-instance = "https://lemmy.world"
-community = "news"
-url = f"{instance}/api/v3/post/list"
 
-params = {"community_name": community, "limit": 1, "sort": "Hot"}
+with open("config.yml", "r") as f:
+    config = yaml.safe_load(f)
 
-resp = requests.get(url, params=params)
-posts = resp.json()["posts"]
+# print(config)
 
-for post in posts:
-    print(post)
+all_posts = []
+
+
+def get_posts(instances):
+    print("Fetching Posts...")
+    for instance in instances:
+        url = f"{instance["url"]}/api/v3/post/list"
+
+        for community in instance["communities"]:
+            params = {
+                "community_name": community["name"],
+                "limit": community["posts"],
+                "sort": "Hot",
+            }
+            print(f"Getting posts from {instance['url']}/c/{community['name']}...")
+            resp = requests.get(url, params=params)
+
+            all_posts.append(resp.json()["posts"])
+            print("Done")
+
+    print("All posts received")
+    for instance_posts in all_posts:
+        for post in instance_posts:
+            print(post["post"]["name"])
+
+
+get_posts(config["instances"])
