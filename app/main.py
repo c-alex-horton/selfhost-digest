dea500b8849caf130c068b8fb0faaf6d7f8100fc
calex <calexch@tuta.com>
2025-05-07 18:20:34 -0500

    Moved config to it's own file

--- app/main.py
+++ app/main.py
@@ -6,20 +6,8 @@ from pathlib import Path
 import json
 import shutil
 from .utils import download_image, handle_opengraph
+from .config import config
 
-config_path = Path("config.yml")
-
-try:
-    with config_path.open("r") as f:
-        config = yaml.safe_load(f)
-except FileNotFoundError:
-    print(
-        f"Config file not found at {config_path.resolve()}. You can create one by copying the config_example.yml and renaming it to 'config.yml'"
-    )
-    sys.exit(1)
-except yaml.YAMLError as e:
-    print(f"Error parsing config file: {e}")
-    sys.exit(1)
 
 all_posts = []
 
